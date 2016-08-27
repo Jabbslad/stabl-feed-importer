11cdeb3f82ad9d836565a03cf12ea256712464a7
Jabbslad <jabbslad@gmail.com>
2016-08-27 21:53:07 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var parser = require("parse-rss");
+var firebase = require("firebase");
+var request = require('request-promise');
+
+if (process.argv.length !== 5) {
+	console.log("usage: node app.js <auth path> <db address> <itunes url>")
+	process.exit(1)
+} 
+
+firebase.initializeApp({
+  serviceAccount: process.argv[2],
+  databaseURL: process.argv[3]
+});
+
+function parseFeed(rss) {
+
+	var first = rss[0];
+    var meta = first.meta;
+    var parsed = {};
+    parsed.feedUrl = meta.xmlUrl;
+    parsed.collectionName = meta.title;
+    parsed.title = first.title;
+    parsed.link = first.link;
+    parsed.guid = first.guid;
+    parsed.pubDate = Date.parse(first.pubDate);
+    parsed.categories = meta.categories;
+    parsed.author = first.author;
+    parsed.thumbnail = first.image.url || meta.image.url || '';
+    parsed.description = first.description;
+    parsed.enclosure = first["media:content"]['@'];
+
+    return parsed;
+}
+
+function parseFeedUrl(url) {
+	return new Promise(function(resolve, reject) {
+		parser(url, function(err, rss) {
+			if(err) reject(err);
+			firebase.database().ref('items').push(parseFeed(rss))
+			.then(function(val) {
+				resolve(val);
+			}).catch(function(reason) {
+				reject(reason);
+			});
+		});
+	})
+}
+
+function fetch(url) {
+	return new Promise(function(resolve, reject) {
+		request({method: 'GET', uri: url})
+		.then(function (response) {
+	    	var parsed = JSON.parse(response);
+	    	if (parsed.resultCount !== 1) {
+	    		reject(Error('Expected 1 result, found ' + parsed.resultCount));
+	    	}
+	    	var feedUrl = parsed.results[0].feedUrl;
+	    	resolve(feedUrl);
+	  	})
+	  	.catch(function (err) {
+	  		reject(err);
+	  	});
+	})
+}
+
+fetch(process.argv[4])
+	.then(parseFeedUrl)
+	.then(function(val) { 
+		console.log("Added: " + val.key);
+		process.exit(0);
+	})
+	.catch(function(msg) {
+		console.log(msg);
+		process.exit(1);
+	});
