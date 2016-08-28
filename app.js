a5b58793b057ab35dc7e396f8588a404ddaf44fc
Jabbslad <jabbslad@gmail.com>
2016-08-28 20:56:15 +0100

    updated to use stable-rss-to-json

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 (function () {
     "use strict";
-    const parser = require("parse-rss");
+    const rss = require("stabl-rss-to-json");
     const firebase = require("firebase");
-    const request = require('request-promise');
+    const _ = require("underscore");
 
     if (process.argv.length !== 5) {
         console.log("usage: node app.js <auth path> <db address> <itunes url>");
@@ -14,60 +14,30 @@
         databaseURL: process.argv[3]
     });
 
-    function parseFeed(rss) {
-        const first = rss[0];
-        var meta = first.meta;
-        var parsed = {};
-        parsed.feedUrl = meta.xmlUrl;
-        parsed.collectionName = meta.title;
-        parsed.title = first.title;
-        parsed.link = first.link;
-        parsed.guid = first.guid;
-        parsed.pubDate = Date.parse(first.pubDate);
-        parsed.categories = meta.categories;
-        parsed.author = first.author;
-        parsed.thumbnail = first.image.url || meta.image.url || '';
-        parsed.description = first.description;
-        parsed.enclosure = first["media:content"]['@'];
-
-        return parsed;
+    function normalize(item) {
+        item.pubDate = Date.parse(item.pubDate);
+        return item;
     }
 
-    function parseFeedUrl(url) {
+    function fetch(id) {
         return new Promise(function (resolve, reject) {
-            parser(url, function (err, rss) {
-                if (err) {
-                    reject(err);
-                }
-                firebase.database().ref('items').push(parseFeed(rss))
+            rss.feedUrlFromItunesCollectionId(id)
+                .then(rss.parseFeedUrl)
+                .then(feed => {
+                    var item = _.first(feed);
+                    firebase.database().ref('items').push(normalize(item))
                     .then(function (val) {
                         resolve(val);
                     }).catch(function (reason) {
                         reject(reason);
                     });
-            });
-        });
-    }
-
-    function fetch(url) {
-        return new Promise(function (resolve, reject) {
-            request({method: 'GET', uri: url})
-                .then(function (response) {
-                    var parsed = JSON.parse(response);
-                    if (parsed.resultCount !== 1) {
-                        reject(new Error('Expected 1 result, found ' + parsed.resultCount));
-                    }
-                    var feedUrl = parsed.results[0].feedUrl;
-                    resolve(feedUrl);
-                })
-                .catch(function (err) {
+                }).catch(function (err) {
                     reject(err);
                 });
         });
     }
 
     fetch(process.argv[4])
-        .then(parseFeedUrl)
         .then(function (val) {
             console.log("Added: " + val.key);
             process.exit(0);
