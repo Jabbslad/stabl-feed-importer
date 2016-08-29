0e02e87ef0d4c3bf99b7e9bfad54fca745420682
Jabbslad <jabbslad@gmail.com>
2016-08-29 16:31:08 +0100

    migrated to parse

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 (function () {
     "use strict";
     const rss = require("stabl-rss-to-json");
-    const firebase = require("firebase");
+    const Parse = require("parse/node");
     const _ = require("underscore");
 
-    if (process.argv.length !== 5) {
-        console.log("usage: node app.js <auth path> <db address> <itunes url>");
+    if (process.argv.length !== 7) {
+        console.log("usage: node app.js <user> <pass> <app id> <server url> <itunes collection id>");
         process.exit(1);
     }
 
-    firebase.initializeApp({
-        serviceAccount: process.argv[2],
-        databaseURL: process.argv[3]
-    });
+    Parse.initialize(process.argv[4]);
+    Parse.serverURL = process.argv[5]
 
-    function normalize(item) {
-        item.pubDate = Date.parse(item.pubDate);
-        return item;
+    function save(item) {
+        return Parse.User.logIn(process.argv[2], process.argv[3]).then(user => {
+            var PodcastItem = Parse.Object.extend("PodcastItem");
+            var podcastItem = new PodcastItem();
+            return podcastItem.save(item, {sessionToken : user.getSessionToken()});
+        })
     }
 
     function fetch(id) {
@@ -25,21 +26,15 @@
                 .then(rss.parseFeedUrl)
                 .then(feed => {
                     var item = _.first(feed);
-                    firebase.database().ref('items').push(normalize(item))
-                    .then(val => {
-                        resolve(val);
-                    }).catch(reason => {
-                        reject(reason);
-                    });
-                }).catch(err => {
-                    reject(err);
-                });
+                    resolve(save(item));
+                })
+                .catch(reject);
         });
     }
 
-    fetch(process.argv[4])
+    fetch(process.argv[6])
         .then(val => {
-            console.log("Added: " + val.key);
+            console.log(val);
             process.exit(0);
         })
         .catch(msg => {
